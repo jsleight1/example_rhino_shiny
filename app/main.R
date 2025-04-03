4ddb9781e8b9a474646af1d58a5365891b2dcaa9
jsleight1 <50985034+jsleight1@users.noreply.github.com>
2025-04-03 15:11:17 +0100

    Update main.R

--- app/main.R
+++ app/main.R
@@ -11,34 +11,34 @@ box::use(
 
 data("rhinos", package = "rhino", envir = environment())
 
-#' @export
-ui <- function(id) {
-  ns <- NS(id)
+# #' @export
+# ui <- function(id) {
+#   ns <- NS(id)
 
-  bootstrapPage(
-    h1("RhinoApplication"),
-    div(
-      class = "components-container",
-      table$ui(ns("table")),
-      chart$ui(ns("chart"))
-    ),
-    div(
-      class = "help",
-      tags$button(
-        id = "help-button",
-        icon("question"),
-        onclick = "App.showHelp()"
-      )
-    )
-  )
-}
+#   bootstrapPage(
+#     h1("RhinoApplication"),
+#     div(
+#       class = "components-container",
+#       table$ui(ns("table")),
+#       chart$ui(ns("chart"))
+#     ),
+#     div(
+#       class = "help",
+#       tags$button(
+#         id = "help-button",
+#         icon("question"),
+#         onclick = "App.showHelp()"
+#       )
+#     )
+#   )
+# }
 
-#' @export
-server <- function(id) {
-  moduleServer(id, function(input, output, session) {
-    data <- rhinos
+# #' @export
+# server <- function(id) {
+#   moduleServer(id, function(input, output, session) {
+#     data <- rhinos
 
-    table$server("table", data = data)
-    chart$server("chart", data = data)
-  })
-}
+#     table$server("table", data = data)
+#     chart$server("chart", data = data)
+#   })
+# }
